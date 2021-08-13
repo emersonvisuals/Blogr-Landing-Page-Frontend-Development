9b85e4e1a0761199e68981808a177ea56b161358
Thomas Emerson <emersonvisuals@gmail.com>
2021-08-13 15:31:38 +1000

    JS attempt

--- blogr-landing-page-main/main.js
+++ blogr-landing-page-main/main.js
@@ -1,25 +1,49 @@
-const products = document.querySelector('.products');
-const productsArrow = document.querySelector('.arrow-01');
-
 // active Products
-products.addEventListener('click',() => {
-    productsArrow.classList.toggle('arrow-active');
-});
+const productsOn = () => {
+    const products = document.querySelector('.products');
+    const productsArrow = document.querySelector('.arrow-01');
+    const productsDropdown = document.querySelector('.products-dropdown');
 
-const company = document.querySelector('.company');
-const companyArrow = document.querySelector('.arrow-02');
+    products.addEventListener('click',() => {
+        productsArrow.classList.toggle('arrow-active');
+        productsDropdown.classList.toggle('products-dropdown-active');
+    });
+}
 
-//active Company
-company.addEventListener('click',() =>{
-    companyArrow.classList.toggle('arrow-active');
-});
-
-const connect = document.querySelector('.connect');
-const connectArrow = document.querySelector('.arrow-03');
-
-//active Connect
-connect.addEventListener('click',()=>{
-    connectArrow.classList.toggle('arrow-active');
-});
+productsOn();
 
 
+
+
+// active Company
+const companyOn = () => {
+    const company = document.querySelector('.company');
+    const companyArrow = document.querySelector('.arrow-02');
+    const companyDropdown = document.querySelector('.company-dropdown')
+
+    company.addEventListener('click',() =>{
+        companyArrow.classList.toggle('arrow-active');
+        companyDropdown.classList.toggle('company-dropdown-active');
+    });
+}
+
+companyOn();
+
+
+
+
+//active Connect 
+const connectOn = () => {
+    const connect = document.querySelector('.connect');
+    const connectArrow = document.querySelector('.arrow-03');
+    const connectDropdown = document.querySelector('.connect-dropdown');
+
+
+    connect.addEventListener('click',()=>{
+        connectArrow.classList.toggle('arrow-active');
+        connectDropdown.classList.toggle('connect-dropdown-active');
+
+    });
+}
+
+connectOn();
